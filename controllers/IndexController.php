0d21f6525cc0729568d8bb1f29e6420f0fa8f524
Xafree <emmanueleberst@hotmail.com>
2019-12-31 01:21:50 +0100

    modification des pages ajout du tableau dans medecin

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -1,97 +1,107 @@
 <?php
-        class GestionDataBase {
+    class GestionDataBase {
 
-            private $login;
-            private $pw;
-            private $serveur;
-            private $database;
-            private $pdo;
+        private $login;
+        private $pw;
+        private $serveur;
+        private $database;
+        private $pdo;
 
-            public function __construct($log,$pass,$server,$db){
-                $this->login = $log;
-                $this->pw = $pass;
-                $this->serveur = $server;
-                $this->database = $db;
+        public function __construct($log,$pass,$server,$db){
+            $this->login = $log;
+            $this->pw = $pass;
+            $this->serveur = $server;
+            $this->database = $db;
+        }
+
+        public function tryConnexion(){
+            try {  
+                $this->pdo = new PDO("mysql:host=$this->serveur;dbname=$this->database", $this->login, $this->pw);
+            }catch (Exception $e) {
+                die('Erreur : '. $e->getMessage());     
+            }
+        }
+
+        private function getValuePostClient(){
+            $out['nom']             = $_POST['user_nom'];
+            $out['prenom']          = $_POST['user_prenom'];
+            $out['adresse']         = $_POST['user_adresse'];
+            $out['ville']           = $_POST['user_ville'];
+            $out['codepostal']      = $_POST['user_code_postal'];
+            $out['civiliter']       = $_POST['user_civile'];
+            $out['naissance']       = $_POST['user_naissance'];
+            $out['lieuNaissance']   = $_POST['user_lieu_naissance'];
+            $out['numsecu']         = $_POST['user_num_secu'];
+            $out['id_medecin']      = $_POST['user_id_medecin'];
+            return $out;
             }
 
-            public function tryConnexion(){
-                try {  
-                    $this->pdo = new PDO("mysql:host=$this->serveur;dbname=$this->database", $this->login, $this->pw);
-                    echo "Connexion successfull \n";
-                }catch (Exception $e) {
-                    die('Erreur : '. $e->getMessage());     
-                }
-            }
+        private function getValuePostMedecin(){
+            $out['nom']         = $_POST['med_nom'];
+            $out['prenom']      = $_POST['med_prenom'];
+            $out['civiliter']   = $_POST['med_civilite'];
+            return $out;
+        }
 
-            private function getValuePostClient(){
-                $out['nom']             = $_POST['user_nom'];
-                $out['prenom']          = $_POST['user_prenom'];
-                $out['adresse']         = $_POST['user_adresse'];
-                $out['ville']           = $_POST['user_ville'];
-                $out['codepostal']      = $_POST['user_code_postal'];
-                $out['civiliter']       = $_POST['user_civile'];
-                $out['naissance']       = $_POST['user_naissance'];
-                $out['lieuNaissance']   = $_POST['user_lieu_naissance'];
-                $out['numsecu']         = $_POST['user_num_secu'];
-                $out['id_medecin']      = $_POST['user_id_medecin'];
-                return $out;
-            }
+        public function insertPatient(){
+            $out = $this->getValuePostClient();
+            $this->tryConnexion();
+            $req = $this->pdo->prepare('INSERT INTO patient(num_securite_social,nom,prenom,adresse,code_postal,date_naissance,civilite,lieu_naissance,id_medecin, ville)
+                                        VALUES(:num_securite_social, :nom, :prenom, :adresse, :code_postal, :date_naissance ,:civilite, :lieu_naissance, :id_medecin, :ville)'); 
+            $req->execute(array('num_securite_social'   => $out['numsecu'],
+                                'nom'                   => $out['nom'],
+                                'prenom'                => $out['prenom'], 
+                                'adresse'               => $out['adresse'],
+                                'code_postal'           => $out['codepostal'],
+                                'date_naissance'        => $out['naissance'],
+                                'civilite'              => $out['civiliter'],
+                                'lieu_naissance'        => $out['lieuNaissance'], 
+                                'id_medecin'            => $out['id_medecin'],
+                                'ville'                 => $out['ville']));
+        }
 
-            private function getValuePostMedecin(){
-                $out['nom']         = $_POST['med_nom'];
-                $out['prenom']      = $_POST['med_prenom'];
-                $out['civiliter']   = $_POST['med_civilite'];
-                return $out;
-            }
-
-            public function insertPatient(){
-                $out = $this->getValuePostClient();
-                $this->tryConnexion();
-                $req = $this->pdo->prepare('INSERT INTO patient(num_securite_social,nom,prenom,adresse,code_postal,date_naissance,civilite,lieu_naissance,id_medecin, ville)
-                                            VALUES(:num_securite_social, :nom, :prenom, :adresse, :code_postal, :date_naissance ,:civilite, :lieu_naissance, :id_medecin, :ville)'); 
-                $req->execute(array('num_securite_social'   => $out['numsecu'],
-                                    'nom'                   => $out['nom'],
-                                    'prenom'                => $out['prenom'], 
-                                    'adresse'               => $out['adresse'],
-                                    'code_postal'           => $out['codepostal'],
-                                    'date_naissance'        => $out['naissance'],
-                                    'civilite'              => $out['civiliter'],
-                                    'lieu_naissance'        => $out['lieuNaissance'], 
-                                    'id_medecin'            => $out['id_medecin'],
-                                    'ville'                 => $out['ville']));
-            }
-
-            public function insertMedecin(){
-                $out = $this->getValuePostMedecin();
-                $this->tryConnexion();
-                $req = $this->pdo->prepare('INSERT INTO medecin (nom, prenom, civilite)
-                                            VALUES(:nom, :prenom,:civilite)');  
-                $req->execute(array('nom'       => $out['nom'],
-                                    'prenom'    => $out['prenom'],
-                                    'civilite'  => $out['civiliter']));
-            } 
+        public function insertMedecin(){
+            $out = $this->getValuePostMedecin();
+            $this->tryConnexion();
+            $req = $this->pdo->prepare('INSERT INTO medecin (nom, prenom, civilite)
+                                        VALUES(:nom, :prenom,:civilite)');  
+            $req->execute(array('nom'       => $out['nom'],
+                                'prenom'    => $out['prenom'],
+                                'civilite'  => $out['civiliter']));
+        } 
             
-            public function requetePatient(){
-                $requete = "SELECT * FROM `patient`";
-                $this->tryConnexion();
-                
+        public function requetePatient(){
+            $requete = "SELECT * FROM `patient`";
+            $this->tryConnexion();        
+        }
 
+        public function deleteMed($med){
+            $chek = "SELECT nom from `medecin` where `medecin`.`nom` = '$med";
+            $sql = "DELETE FROM `medecin` WHERE `medecin`.`nom` = '$med'";
+            $this->tryConnexion();
+            $resChek = $this->pdo->query($chek);
+            if($resChek == false){
+                $this->pdo->query($sql);
+            }else{
+                echo " nous avons pas ce nom dans la base de donnée ";
+            }   
+        }
+
+        public function gestionTableauMedecin(){
+            $sql = "SELECT * FROM `medecin`";
+            $this->tryConnexion();
+            try {
+                $stmt = $this->pdo->prepare($sql, array(PDO::ATTR_CURSOR, PDO::CURSOR_SCROLL));
+                $stmt->execute();
+                $row = $stmt->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_LAST);
+                do {
+                    echo "<tr><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td></tr>";
+                } while ($row = $stmt->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_PRIOR));
+                $stmt = null;
             }
-
-            public function deleteMed($med){
-                $chek = "SELECT nom from `medecin` where `medecin`.`nom` = '$med";
-                $sql = "DELETE FROM `medecin` WHERE `medecin`.`nom` = '$med'";
-                $this->tryConnexion();
-                $resChek = $this->pdo->query($chek);
-                var_dump($resChek);
-                    echo $med."\n";
-                if($resChek == false){ // comprend pas pourquoi ca renvoie faux 
-                    $this->pdo->query($sql);
-                }else{
-                    echo " nous avons pas ce nom dans la base de donné ";
-                }     
-                
+            catch (PDOException $e) {
+                print $e->getMessage();
             }
-
-        };
+        }
+    };
 ?>
--- medecin.php
+++ medecin.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <title>gestion medecin</title>
         <link rel="stylesheet" href="">
+        <?php require 'controllers/IndexController.php';?>
     </head>
     <body>
         <div id="globale">
@@ -15,7 +16,7 @@
                 </div>
                 <div id="menu">  
                     <ul id="navigation">
-                        <li><a href="index.html" title="acceuil">Acceuil</a></li>
+                        <li><a href="index.html"  title="acceuil">Acceuil</a></li>
                         <li><a href="medecin.php" title="Medecin">Médecin</a></li>
                         <li><a href="patient.php" title="patient">Patient</a></li>
                     </ul>
@@ -27,42 +28,64 @@
                 <input type="submit" id="Effacer"  name="button_effacer"   value="Effacer" >
             </form>
             <?php 
-            if(isset($_POST['button_ajouter'])){
-                echo'
-                    <h1>Le formulaire des médecin </h1>
-                    <form action="controllers/connexion.php" method="POST">
-                        <div>
-                            <label for="nom">Nom :</label>
-                            <input type="text" id="nom" name="med_nom" >
-                        </div>
-                        <div>
-                            <label for="Prenom">Prénom :</label>
-                            <input type="text" id="Prénom" name="med_prenom" >
-                        </div>
-                        <div>
-                            <label for="civilité">civilité :</label>
-                            <input type="text" id="Adresse" name="med_civilite" >
-                        </div>
-            
-                        <input type="submit" id="envoyer" name="med_valider" value="Valider">
-                        <input type="reset" id="reset" name="med_effacer" value="Effacer">
-                    </form>';}
-                    
-                    if(isset($_POST['button_effacer'])){
-                        echo'
-                        <h1>suppresion de medecin </h1>
+                if(isset($_POST['button_ajouter'])){
+                    echo'
+                        <h1>Le formulaire des médecin </h1>
                         <form action="controllers/connexion.php" method="POST">
                             <div>
                                 <label for="nom">Nom :</label>
                                 <input type="text" id="nom" name="med_nom" >
                             </div>
-                            <input type="submit" id="envoyer" name="suppr_med" value="Valider">
+                            <div>
+                                <label for="Prenom">Prénom :</label>
+                                <input type="text" id="Prénom" name="med_prenom" >
+                            </div>
+                            <div>
+                                <label for="civilité">civilité :</label>
+                                <input type="text" id="Adresse" name="med_civilite" >
+                            </div>
+                
+                            <input type="submit" id="envoyer" name="med_valider" value="Valider">
+                            <input type="reset" id="reset" name="med_effacer" value="Effacer">
                         </form>';
-                        if(isset($_POST['suppr_med'])){
-                            echo "Effacement avec succées";
-                        }  
-                    }
+                }
+
+                if(isset($_POST['button_effacer'])){
+                    echo'
+                    <h1>suppresion de medecin </h1>
+                    <form action="controllers/connexion.php" method="POST">
+                        <div>
+                            <label for="nom">Nom :</label>
+                            <input type="text" id="nom" name="med_nom" >
+                        </div>
+                        <input type="submit" id="envoyer" name="suppr_med" value="Valider">
+                    </form>';
+  
+                }                  
             ?>
+            <br/>
+            <table>
+                <thead>
+                    <tr>
+                        <th colspan = "2">Tableau de medecin</th>
+                    </tr>
+                </thead>
+                <tbody> 
+                    <?php
+                        $serveur ='localhost';
+                        $db = 'bd_projet';
+                        $login = 'root';
+                        $mdp = ''; 
+                        $connexion = new GestionDataBase($login,$mdp,$serveur,$db);
+                        if(isset($_POST['med_valider'])||isset($_POST['suppr_med'])){
+                            echo $connexion->gestionTableauMedecin();
+                        }else{
+                            echo $connexion->gestionTableauMedecin();
+                        }
+                    ?>
+                </tbody>
+            </table>
+
             <footer id="footer">
 
             </footer>
